3a23febf800b7100adc791e5a638421f1c7f2b58
Lucas M Guima <lucasdamata01@hotmail.com>
2025-02-05 10:00:33 -0300

    Exercicio finálizado

--- Ex_01/server.js
+++ Ex_01/server.js
@@ -1,6 +1,32 @@
 import { createServer } from 'http';
+import { parse } from 'url';
+import { readFileSync, readFile } from 'fs';
+
+function successPrint(str, res){
+  res.writeHead(200, { 'Content-type':'text/plain' });
+  res.end(str);
+}
 
 export const server = createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello There!\nGeneral Kenobi');
+    let url = parse(req.url, true);
+    console.log(url);
+
+    switch (url.pathname){
+      case '/teste':
+        successPrint('Teste deu ok.', res);
+        break;
+      case '/nome':
+        let usuario = url.query.usuario
+        successPrint(`Olá ${usuario}.`, res);
+        break;
+      case '/arquivosync':
+        let data = readFileSync('./arquivo.txt');
+        successPrint(data, res);
+        break;
+      case '/arquivoasync':
+        readFile('./arquivo.txt', (error, data) => {
+          successPrint(data, res);
+        });
+        break;
+    }
   });
